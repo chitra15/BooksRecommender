8dd2eb60e996beb189db6f60f4ab72f77f59a402
Chitra Nundlall <shwetanundlall@gmail.com>
2017-02-05 22:13:47 +0400

    31-01-2017

--- SignInUp/categorymain.py
+++ SignInUp/categorymain.py
@@ -66,7 +66,7 @@ Builder.load_string('''
                         text: 'Btn0'
                         icon: 'plus1.png'
                         halign: 'left'
-                        on_press: root.open()
+                        on_press: root.select()
 
                     ActionButton:
                         text: 'Btn1'
@@ -80,32 +80,127 @@ Builder.load_string('''
         BoxLayout:
             size_hint_y: 400
 
+<SelectPopup>:
+    title: 'Category of Books'
+    size_hint: None, None
+    size: 500, 500
+    auto_dismiss: True
+    GridLayout:
+        cols: 2
+        Label:
+            text: 'Juvenile Fiction'
+        CheckBox:
+            on_active: root.add('Juvenile Fiction')
+        Label:
+            text: 'Young Adult Fiction'
+        CheckBox:
+            on_active: root.add('Young Adult Fiction')
+        Label:
+            text: 'Performing Arts'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Drama'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Literary Criticism'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Children'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Contests'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Law'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Social Science'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Study Aids'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Juvenile Nonfiction'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Biography & Autobiography'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Business & Economics'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Man-woman relationships'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'History'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Psychology'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+        Label:
+            text: 'Technology& Engineering'
+        CheckBox:
+            on_active: app.root.color = 'Blue'
+
         ''')
+class SelectPopup(Popup):
+    def add(self, x):
+        print(x)
+
 
 class profile(Screen):
-    dropbtn= ObjectProperty()
+    def select(self):
+        SelectPopup().open()
 
-    def open(self):
-        dropdown = DropDown()
-        dropdown.auto_width= False
 
-        items = ['Juvenile Fiction', 'Young Adult\nFiction', 'Performing Arts', 'Drama', 'Literary Criticism',
-                 'Children', 'Contests',
-                 'Law', 'Social Science', 'Study Aids', 'Juvenile\nNonfiction', 'Biography\n& Autobiography',
-                 'Business\n& Economics', 'Man-woman\nrelationships',
-                 'History', 'Psychology', 'Pressure vessels', 'Architecture', 'Rivets and\nriveting',
-                 'Technology\n& Engineering', 'Stream\nmeasurements']
 
-        for item in items:
-            btn = Button(text= item, size_hint_y=None, height=40, size_hint_x=2, width= 200, font_size= '12sp')
-            dropdown.add_widget(btn)
-            btn.bind(on_press= self.add())
-        actionBtn= self.dropbtn
-        actionBtn.bind(on_release= dropdown.open)
-        dropdown.bind(on_select=lambda instance, x: setattr(actionBtn, 'text', x))
 
-    def add(self):
-        conn = sqlite3.connect('booksrecommender.db')
+
+
+    # dropbtn= ObjectProperty()
+    #
+    # def open(self):
+    #     dropdown = DropDown()
+    #     dropdown.auto_width= False
+    #
+    #     items = ['Juvenile Fiction', 'Young Adult\nFiction', 'Performing Arts', 'Drama', 'Literary Criticism',
+    #              'Children', 'Contests',
+    #              'Law', 'Social Science', 'Study Aids', 'Juvenile\nNonfiction', 'Biography\n& Autobiography',
+    #              'Business\n& Economics', 'Man-woman\nrelationships',
+    #              'History', 'Psychology', 'Pressure vessels', 'Architecture', 'Rivets and\nriveting',
+    #              'Technology\n& Engineering', 'Stream\nmeasurements']
+    #
+    #     for item in items:
+    #         btn = Button(text= item, size_hint_y=None, height=40, size_hint_x=2, width= 200, font_size= '12sp')
+    #         dropdown.add_widget(btn)
+    #
+    #     actionBtn= self.dropbtn
+    #     actionBtn.bind(on_release= dropdown.open)
+    #
+    #
+    # def add(self):
+    #     popup = Popup(title="Information", content=Label(text="Welcome"), size_hint=(None, None), size=(400, 200))
+    #     popup.open()
+    #     # conn = sqlite3.connect('booksrecommender.db')
+
+
+
+
+
 
 
 
